a3b857b8cd7d6bd9bd56057db6aa93957d447966
Abinaya <abiselvam080@gmail.com>
2022-03-25 19:51:02 +0530

    commit four

---
+++ 10-02-2022/src/Arithmetic.java
@@ -0,0 +1,24 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+	
+		int a=10;
+		int b=11;
+		int c=12;
+		
+		int add=a+b+c;
+		int sub=c-a;
+		int mul=a*b;
+		int div=c/3;
+		int rem=c%a;
+		
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
+		System.out.println(rem);
+
+	}
+
+}
---
+++ 10-02-2022/src/Unaryoperators.java
@@ -0,0 +1,32 @@
+
+public class Unaryoperators {
+
+	public static void main(String[] args) {
+		
+		//using unary plus operator
+		int number=+100;
+		System.out.println(number);
+		
+		//using unary minus operator
+		number=-number;
+		System.out.println(number);
+		
+		//increments the value of variable by 1
+		int a=20;
+		a++;
+		System.out.println(a);
+		
+		//decrements the value of variable by 1
+		a--;
+		System.out.println(a);
+		
+		//declaring a boolean variable
+		boolean flag=false;
+		//prints the value of success variable
+		System.out.println(flag);
+		//prints the complement of sucess variable
+		System.out.println(!flag);
+	
+	}
+
+}
